5565a43d46d7fbbac66e19b9300ee6ceb0522a32
Federico_Campanozzi <federico.campanozzi@gmail.com>
2023-10-12 23:13:40 +0200

    Try command "dotnet ef migrations add NewHeroColumns"

---
+++ backend/API/Migrations/20231011222656_NewHeroColumns.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewHeroColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreate",
+                table: "SuperHeroes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "SuperHeroes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMainCharacter",
+                table: "SuperHeroes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastDateUpdate",
+                table: "SuperHeroes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreate",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropColumn(
+                name: "IsMainCharacter",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropColumn(
+                name: "LastDateUpdate",
+                table: "SuperHeroes");
+        }
+    }
+}
